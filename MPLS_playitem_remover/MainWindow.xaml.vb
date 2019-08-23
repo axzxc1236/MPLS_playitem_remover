63ccb82d135b763e53f50007ba99b03c0358607f
axzxc1236 <axzxc1236@gmail.com>
2019-08-23 18:19:52 +0800

    Allow drag and drop for mpls file and tweak for config file format.

--- MPLS_playitem_remover/MainWindow.xaml.vb
+++ MPLS_playitem_remover/MainWindow.xaml.vb
@@ -6,33 +6,39 @@
     Dim length_of_the_playlist_section As Long
     Dim number_of_play_items As Integer
     Dim playlist_items As New List(Of Playlist_item)
-    Dim dialogue As New Microsoft.Win32.OpenFileDialog
+    Dim filePathToSaveModifiedFile As String = ""
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim dialogue As New Microsoft.Win32.OpenFileDialog
         dialogue.Filter = "MPLS playlist (*.mpls)|*.mpls"
         If dialogue.ShowDialog = True Then
-            m2tsList.Items.Clear()
-            filecontent = My.Computer.FileSystem.ReadAllBytes(dialogue.FileName)
-            playlist_start_address = filecontent(8) * 16777216 + filecontent(9) * 65536 + filecontent(10) * 256 + filecontent(11)
-            playlist_mark_start_address = filecontent(12) * 16777216 + filecontent(13) * 65536 + filecontent(14) * 256 + filecontent(15)
-            length_of_the_playlist_section = filecontent(playlist_start_address) * 16777216 + filecontent(playlist_start_address + 1) * 65536 + filecontent(playlist_start_address + 2) * 256 + filecontent(playlist_start_address + 3)
-            number_of_play_items = filecontent(playlist_start_address + 6) * 256 + filecontent(playlist_start_address + 7)
-            startindex = playlist_start_address + 10
-            For i As Integer = 1 To number_of_play_items
-                Dim item As New Playlist_item
-                item.length = filecontent(startindex) * 256 + filecontent(startindex + 1)
-                item.content = New Byte(item.length) {}
-                startindex += 2
-                For j As Long = 0 To item.length - 1
-                    item.content(j) = filecontent(startindex + j)
-                Next
-                For j = 0 To 8
-                    item.filename &= Convert.ToChar(item.content(j))
-                Next
-                startindex += item.length
-                m2tsList.Items.Add(item.filename)
-                playlist_items.Add(item)
-            Next
+            loadMPLSfile(dialogue.FileName)
+            filePathToSaveModifiedFile = Text.RegularExpressions.Regex.Replace(dialogue.FileName, "\\\d{5}.mpls", "\modified.mpls")
         End If
+    End Sub
+
+    Sub loadMPLSfile(filename As String)
+        m2tsList.Items.Clear()
+        filecontent = My.Computer.FileSystem.ReadAllBytes(filename)
+        playlist_start_address = filecontent(8) * 16777216 + filecontent(9) * 65536 + filecontent(10) * 256 + filecontent(11)
+        playlist_mark_start_address = filecontent(12) * 16777216 + filecontent(13) * 65536 + filecontent(14) * 256 + filecontent(15)
+        length_of_the_playlist_section = filecontent(playlist_start_address) * 16777216 + filecontent(playlist_start_address + 1) * 65536 + filecontent(playlist_start_address + 2) * 256 + filecontent(playlist_start_address + 3)
+        number_of_play_items = filecontent(playlist_start_address + 6) * 256 + filecontent(playlist_start_address + 7)
+        startindex = playlist_start_address + 10
+        For i As Integer = 1 To number_of_play_items
+            Dim item As New Playlist_item
+            item.length = filecontent(startindex) * 256 + filecontent(startindex + 1)
+            item.content = New Byte(item.length) {}
+            startindex += 2
+            For j As Long = 0 To item.length - 1
+                item.content(j) = filecontent(startindex + j)
+            Next
+            For j = 0 To 8
+                item.filename &= Convert.ToChar(item.content(j))
+            Next
+            startindex += item.length
+            m2tsList.Items.Add(item.filename)
+            playlist_items.Add(item)
+        Next
     End Sub
 
     Private Sub removeButton_Click(sender As Object, e As RoutedEventArgs) Handles removeButton.Click
@@ -64,11 +70,10 @@
         Array.Copy(filecontent, Long.Parse(startindex + 2 + playlist_items.Item(m2tsList.SelectedIndex).length), tmp, 0, newfile.Length - startindex - 2)
         Array.Copy(tmp, 0, newfile, startindex, newfile.Length - startindex)
 
-        Dim newfileLocation = dialogue.FileName.Substring(0, dialogue.FileName.Length - dialogue.SafeFileName.Length) & "modified.mpls"
-        If My.Computer.FileSystem.FileExists(newfileLocation) Then
-            My.Computer.FileSystem.DeleteFile(newfileLocation)
+        If My.Computer.FileSystem.FileExists(filePathToSaveModifiedFile) Then
+            My.Computer.FileSystem.DeleteFile(filePathToSaveModifiedFile)
         End If
-        My.Computer.FileSystem.WriteAllBytes(newfileLocation, newfile, False)
+        My.Computer.FileSystem.WriteAllBytes(filePathToSaveModifiedFile, newfile, False)
         MsgBox("Creadted ""modified.m2ts"" in playlist folder.")
     End Sub
 
@@ -95,7 +100,8 @@
         configText &= sender.Top & Environment.NewLine
         configText &= sender.Left & Environment.NewLine
         configText &= sender.RenderSize.Height & Environment.NewLine
-        configText &= sender.RenderSize.Width
+        configText &= sender.RenderSize.Width & Environment.NewLine
+        configText &= (sender.WindowState = 2).ToString
         Try
             My.Computer.FileSystem.WriteAllText("config", configText, False)
         Catch ex As Exception
@@ -103,19 +109,38 @@
         End Try
     End Sub
 
-    Private Sub onWindowSourceInitialized(sender As Object, e As EventArgs)
+    Private Sub onWindowSourceInitialized(sender As Window, e As EventArgs)
         If My.Computer.FileSystem.FileExists("config") Then
             Dim settings = My.Computer.FileSystem.ReadAllText("config").Split(Environment.NewLine)
-            If settings.Length = 4 Then
+            If settings.Length = 5 Then
                 Try
                     sender.Top = Double.Parse(settings(0))
                     sender.Left = Double.Parse(settings(1))
                     sender.Height = Double.Parse(settings(2))
                     sender.Width = Double.Parse(settings(3))
+                    If settings(4).Contains("True") Then
+                        sender.WindowState = WindowState.Maximized
+                    End If
                 Catch ex As Exception
                     'Do nothing
                 End Try
             End If
+        End If
+    End Sub
+
+    Private Sub onWindowDragEnter(sender As Object, e As DragEventArgs)
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            e.Effects = DragDropEffects.Copy
+        Else
+            e.Effects = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub onWindowsDrop(sender As Object, e As DragEventArgs)
+        Dim fileList As String() = e.Data.GetData(DataFormats.FileDrop)
+        If fileList.Length = 1 AndAlso fileList(0).ToLower().EndsWith(".mpls") AndAlso My.Computer.FileSystem.FileExists(fileList(0)) Then
+            filePathToSaveModifiedFile = Text.RegularExpressions.Regex.Replace(fileList(0), "\\\d{5}.mpls", "\modified.mpls")
+            loadMPLSfile(fileList(0))
         End If
     End Sub
 End Class
